4902159001798e5779bebd193855f79bb9402bca
Travis Exline <t.exline22@gmail.com>
2019-04-30 16:54:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
 def square_array(array)
-  new_array = []
-  array.each do |element|
-    new_array << element ** 2
-    new_array
-  end# your code here
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
